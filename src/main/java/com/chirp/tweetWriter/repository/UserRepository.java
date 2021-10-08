768102910b47c1ec8bf4af0efd66b2f1ad3d01ce
Sourav <souravgopal25@gmail.com>
2021-10-08 11:23:04 +0530

    JwtUtil Service Created

---
+++ src/main/java/com/chirp/tweetWriter/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.chirp.tweetWriter.repository;
+
+import com.chirp.tweetWriter.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
